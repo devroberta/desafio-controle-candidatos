da1013ea98ab0056f1ada46c6810d60d40d1c9aa
Roberta de Siqueira <designer.roberta@gmail.com>
2022-11-06 16:22:15 -0300

    imprimindo uma lista com os candidatos selecionados

--- src/main/java/candidatura/ProcessoSeletivo.java
+++ src/main/java/candidatura/ProcessoSeletivo.java
@@ -6,9 +6,23 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
   public static void main(String[] args) {
     Locale.setDefault(Locale.US);
-    selecaoCandidatos();
+    imprimirSelecionados();
   }
 
+  static void imprimirSelecionados() {
+    String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+
+    System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+    for(int indice=0; indice < candidatos.length; indice++) {
+      System.out.println("O candidato de n° "+ (indice+1) + " é o " + candidatos[indice]);
+    }
+
+    System.out.println("Forma abrevida de interação for each:");
+
+    for(String candidato : candidatos) {
+      System.out.println("O candidato selecionado foi " + candidato);
+    }
+  }
   static void selecaoCandidatos() {
     String[] candidatos =
             {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
