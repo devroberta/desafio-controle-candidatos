d58eb72bea923aaf82b5a78ad76298bb406814ec
Roberta de Siqueira <designer.roberta@gmail.com>
2022-11-06 13:39:09 -0300

    selecionando candidatos

--- src/main/java/candidatura/ProcessoSeletivo.java
+++ src/main/java/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,36 @@
 package candidatura;
 
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
+    Locale.setDefault(Locale.US);
+    selecaoCandidatos();
+  }
+
+  static void selecaoCandidatos() {
+    String[] candidatos =
+            {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+    int candidatosSelecionados = 0;
+    int candidatosAtual = 0;
+    double salarioBase = 2000.0;
+    while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+      String candidato = candidatos[candidatosAtual];
+      double salarioPretendido = valorPretendido();
+
+      System.out.printf("O candidato %s solicitou este valor de salário %.2f\n", candidato, salarioPretendido);
+      if (salarioBase >= salarioPretendido) {
+        System.out.printf("O candidato %s foi selecionado para a vaga.\n", candidato);
+        candidatosSelecionados++;
+      }
+      candidatosAtual++;
+    }
+  }
+
+  static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
   }
 
   static void analisarCandidato(double salarioPretendido) {
